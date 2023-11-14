2bd1f3d7e196b36e4f7f43c75e7863319f9edff0
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s14d75.42wolfsburg.de>
2023-11-14 10:21:40 +0100

    Addition of ft_isprint function

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,22 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/13 23:45:24 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/14 10:20:19 by pabeckha         ###   ########.fr       */
+/*   Created: 2023/11/14 10:10:02 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/11/14 10:20:07 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-
-int	ft_isalpha(int c);
-int	ft_isdigit(int c);
-int	ft_isalnum(int c);
-int	ft_isascii(int c);
-int ft_isprint(int c);
-
-#endif
+int ft_isprint(int c)
+{
+    return (c >= 32 && c < 126);
+}
