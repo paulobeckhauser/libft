a7983a2a95b2d8c08bbf0faad15302a00bb9fd00
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s14d75.42wolfsburg.de>
2023-11-14 10:24:50 +0100

    Norminette corrections

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/13 23:45:24 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/14 10:20:19 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/11/14 10:24:18 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@ int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
-int ft_isprint(int c);
+int	ft_isprint(int c);
 
 #endif
