bcbc608449922b15de3b5ae69da18ab12220f05a
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s14d75.42wolfsburg.de>
2023-11-14 12:55:01 +0100

    Addition of ft_memset function

--- ft_memset.c
+++ ft_memset.c
@@ -1,25 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/13 23:45:24 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/14 12:53:49 by pabeckha         ###   ########.fr       */
+/*   Created: 2023/11/14 10:38:11 by pabeckha          #+#    #+#             */
+/*   Updated: 2023/11/14 12:54:00 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-typedef unsigned long	t_size_t;
-int						ft_isalpha(int c);
-int						ft_isdigit(int c);
-int						ft_isalnum(int c);
-int						ft_isascii(int c);
-int						ft_isprint(int c);
-unsigned long			ft_strlen(char *str);
-void					*ft_memset(void *b, int c, t_size_t len);
+void	*ft_memset(void *b, int c, t_size_t len)
+{
+	unsigned long	i;
+	unsigned char	*ptr;
 
-#endif
+	i = 0;
+	ptr = b;
+	while (i < len)
+	{
+		ptr[i] = (unsigned char)c;
+		i++;
+	}
+	return (b);
+}
