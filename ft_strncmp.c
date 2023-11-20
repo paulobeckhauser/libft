f90d46bbdce8e22209dd52105ea80f6c2ae4cea5
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2s13d71.42wolfsburg.de>
2023-11-20 19:57:32 +0100

    Final version of ft_strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/15 13:08:46 by pabeckha          #+#    #+#             */
-/*   Updated: 2023/11/15 14:59:33 by pabeckha         ###   ########.fr       */
+/*   Updated: 2023/11/20 19:50:41 by pabeckha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	unsigned long	i;
+	size_t	i;
 
 	i = 0;
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		else
 			i++;
 	}
