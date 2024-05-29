59adea8c1242159b25aa51b523c25ed7c3afefc0
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2r4s6.42wolfsburg.de>
2024-05-29 13:11:18 +0200

    Updated Libft version for other projects

---
+++ src/ft_mem/ft_calloc.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sfrankie <sfrankie@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/21 16:22:00 by pabeckha          #+#    #+#             */
+/*   Updated: 2024/04/23 23:25:01 by sfrankie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	char	*ptr;
+	size_t	i;
+
+	ptr = malloc(count * size);
+	if (!ptr)
+		return (NULL);
+	i = 0;
+	while (i < count * size)
+	{
+		ptr[i++] = '\0';
+	}
+	return (ptr);
+}
