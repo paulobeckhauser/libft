1da7db87bdccd0a5e0cd4fda20cb2f1bf65d976e
Paulo Ricardo Beckhauser De Araujo <pabeckha@c2r1s1.42wolfsburg.de.10.in-addr.arpa>
2024-06-01 14:16:50 +0200

    Addition of ft_strcmp to library

---
+++ src/ft_str/ft_strcmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabeckha <pabeckha@student.42wolfsburg.de> +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 14:09:03 by pabeckha          #+#    #+#             */
+/*   Updated: 2024/06/01 14:09:52 by pabeckha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s1[i] || s2[i]))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		else
+			i++;
+	}
+	return (0);
+}
